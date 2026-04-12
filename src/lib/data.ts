7f6a783d273b06693ba4635add0689eaf4c7a3f5
Andrew Zvoma <azvoma@gmail.com>
2026-04-12 21:45:32 +0100

    Updated data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -977,7 +977,7 @@ export const rivers: River[] = [
     latitude: 55.0,
     longitude: -7.3,
     featured: false,
-    image_url: 'https://images.pexels.com/photos/1179229/pexels-photo-1179229.jpeg',
+    image_url: 'https://i.postimg.cc/RVp9rMHM/River-Foyle.jpg',
     created_at: '2024-01-01T00:00:00Z',
     updated_at: '2024-01-01T00:00:00Z'
   },
