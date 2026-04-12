ef52cf09b9e8df39db5d4bdd3991861c3b7ba7ba
Andrew Zvoma <azvoma@gmail.com>
2026-04-12 20:56:33 +0100

    Updated data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -559,7 +559,7 @@ export const rivers: River[] = [
     latitude: 53.8,
     longitude: -1.55,
     featured: false,
-    image_url: 'https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg',
+    image_url: 'https://i.postimg.cc/PJgfhCnT/river-aire-2.jpeg',
     created_at: '2024-01-01T00:00:00Z',
     updated_at: '2024-01-01T00:00:00Z'
   },
