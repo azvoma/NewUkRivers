e2cafb3e5880b9de9ae50154793a3abb061b9e37
Andrew Zvoma <azvoma@gmail.com>
2026-04-12 23:32:38 +0100

    Updated data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -449,7 +449,7 @@ export const rivers: River[] = [
     latitude: 51.7,
     longitude: -0.02,
     featured: false,
-    image_url: 'https://images.pexels.com/photos/1761279/pexels-photo-1761279.jpeg',
+    image_url: 'https://i.postimg.cc/prDRj7Vp/River-Lee.jpg',
     created_at: '2024-01-01T00:00:00Z',
     updated_at: '2024-01-01T00:00:00Z'
   },
