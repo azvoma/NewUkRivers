b87ac597be658e2a74433644f576e12b0f06b5e1
Andrew Zvoma <azvoma@gmail.com>
2026-04-12 20:58:39 +0100

    Updated data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -581,7 +581,7 @@ export const rivers: River[] = [
     latitude: 53.7,
     longitude: -1.5,
     featured: false,
-    image_url: 'https://images.pexels.com/photos/1761279/pexels-photo-1761279.jpeg',
+    image_url: 'https://i.postimg.cc/MTQWWg1t/River-Aire-at-Castleford.jpg',
     created_at: '2024-01-01T00:00:00Z',
     updated_at: '2024-01-01T00:00:00Z'
   },
