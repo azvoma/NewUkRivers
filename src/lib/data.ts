825afc2da19f9b2fbbf481ad57de627397e20dc6
Andrew Zvoma <azvoma@gmail.com>
2026-04-12 21:41:26 +0100

    Updated data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -999,7 +999,7 @@ export const rivers: River[] = [
     latitude: 54.6,
     longitude: -5.9,
     featured: false,
-    image_url: 'https://images.pexels.com/photos/534164/pexels-photo-534164.jpeg',
+    image_url: 'https://i.postimg.cc/yYH3JyQF/River_Lagan.jpg',
     created_at: '2024-01-01T00:00:00Z',
     updated_at: '2024-01-01T00:00:00Z'
   }
