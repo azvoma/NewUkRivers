ac1b0ab54b73ca17a2b6fb3c5e9f00439edf39d5
Andrew Zvoma <azvoma@gmail.com>
2026-04-12 21:07:04 +0100

    Updated data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -625,7 +625,7 @@ export const rivers: River[] = [
     latitude: 53.7,
     longitude: -2.8,
     featured: false,
-    image_url: 'https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg',
+    image_url: 'https://i.postimg.cc/rp2jnrg7/river_ribble_image_card.jpg',
     created_at: '2024-01-01T00:00:00Z',
     updated_at: '2024-01-01T00:00:00Z'
   },
