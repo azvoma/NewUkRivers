eafcbb2edf866aa68113d9f8f802456a91691aaa
Andrew Zvoma <azvoma@gmail.com>
2026-04-12 23:08:45 +0100

    Updated data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -251,7 +251,7 @@ export const rivers: River[] = [
     latitude: 52.2,
     longitude: -1.7,
     featured: false,
-    image_url: 'https://images.pexels.com/photos/1761279/pexels-photo-1761279.jpeg',
+    image_url: 'https://i.postimg.cc/RFK3ZgtV/River-Avon.jpg',
     created_at: '2024-01-01T00:00:00Z',
     updated_at: '2024-01-01T00:00:00Z'
   },
