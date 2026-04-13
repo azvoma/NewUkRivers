5427641f188aad26cfcaf7bfea7f81631f7913f7
Andrew Zvoma <azvoma@gmail.com>
2026-04-13 12:05:02 +0100

    Updated data.ts

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -383,7 +383,7 @@ export const rivers: River[] = [
     latitude: 52.35,
     longitude: 0.2,
     featured: false,
-    image_url: 'https://images.pexels.com/photos/1761279/pexels-photo-1761279.jpeg',
+    image_url: 'https://i.postimg.cc/XJH7TLgx/River_Great_Ouse_Card.jpg',
     created_at: '2024-01-01T00:00:00Z',
     updated_at: '2024-01-01T00:00:00Z'
   },
