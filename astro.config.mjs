a532247b8c7fafd9c7f52c08ab239118ef47990e
Andrew Zvoma <azvoma@gmail.com>
2026-07-02 22:07:37 +0100

    Fix trailing slash redirect loop

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ukrivers.co.uk',
-  trailingSlash: 'never',
+  trailingSlash: 'always',
   integrations: [sitemap()],
 });
