734b0741db0a877bcd2216e08c6e16c9de680bbc
Andrew Zvoma <azvoma@gmail.com>
2026-08-27 10:09:22 +0100

    Added affiliates.ts

---
+++ src/lib/affiliates.ts
@@ -0,0 +1,38 @@
+export const affiliates = {
+  redEquipment: {
+    name: 'Red Equipment',
+    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=REPLACE_ME&awinaffid=REPLACE_ME&p=https://www.red-equipment.co.uk/',
+    ctaText: 'Shop paddleboards & outdoor gear',
+    banners: {
+      leaderboard728x90: '',
+      mobile320x50: '',
+      square400x400: '',
+      rectangle300x250: '',
+      sidebar300x600: '',
+    },
+  },
+  totalFishingTackle: {
+    name: 'Total Fishing Tackle',
+    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=REPLACE_ME&awinaffid=REPLACE_ME&p=https://www.totalfishingtackle.com/',
+    ctaText: 'Shop fishing tackle & waders',
+    banners: {
+      leaderboard728x90: '',
+      mobile320x50: '',
+      square400x400: '',
+      rectangle300x250: '',
+      sidebar300x600: '',
+    },
+  },
+  macdonaldHotels: {
+    name: 'Macdonald Hotels',
+    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=REPLACE_ME&awinaffid=REPLACE_ME&p=https://www.macdonaldhotels.co.uk/',
+    ctaText: 'Check hotel availability near this river',
+    banners: {
+      leaderboard728x90: '',
+      mobile320x50: '',
+      square400x400: '',
+      rectangle300x250: '',
+      sidebar300x600: '',
+    },
+  },
+};
