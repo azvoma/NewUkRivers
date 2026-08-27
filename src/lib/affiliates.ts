09cd92b0d4498040dfd31fc55c71f46979614527
Andrew Zvoma <azvoma@gmail.com>
2026-08-27 20:42:36 +0100

    Update affiliate tracking URLs

--- src/lib/affiliates.ts
+++ src/lib/affiliates.ts
@@ -1,47 +1,64 @@
 export const affiliates = {
   redEquipment: {
     name: 'Red Equipment',
-    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=REPLACE_ME&awinaffid=REPLACE_ME&p=https://www.red-equipment.co.uk/',
+    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=27393&awinaffid=3008603',
     ctaText: 'Shop paddleboards & outdoor gear',
     approvedSizes: ['300x250', '728x90', '468x60', '300x600', '250x250', '970x250', '234x60', '200x200', '120x600', '120x20'],
     banners: {
       leaderboard728x90: '',
       mobile320x50: '',
-      square300x250: '',
+      square300x250: 'https://www.awin1.com/cread.php?s=4860022&v=27393&q=440121&r=3008603',
       sidebar300x600: '',
+    },
+    tracking: {
+      square300x250: 'https://www.awin1.com/cread.php?s=4860022&v=27393&q=440121&r=3008603',
+      textLink: 'https://www.awin1.com/cread.php?awinmid=27393&awinaffid=3008603',
     },
   },
   fishingTackleBait: {
     name: 'Fishing Tackle & Bait',
-    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=REPLACE_ME&awinaffid=REPLACE_ME&p=https://www.fishingtackleandbait.co.uk/',
+    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=45595&awinaffid=3008603',
     ctaText: 'Shop fishing tackle & bait',
     approvedSizes: ['728x90', '468x60', '320x100', '320x50', '300x600', '300x250', '300x50', '250x250', '216x36', '200x200', '168x28', '120x600', '120x20'],
     banners: {
-      leaderboard728x90: '',
-      mobile320x50: '',
-      square300x250: '',
+      leaderboard728x90: 'https://www.awin1.com/cread.php?s=4069162&v=45595&q=533136&r=3008603',
+      mobile320x50: 'https://www.awin1.com/cread.php?s=4069162&v=45595&q=533136&r=3008603',
+      square300x250: 'https://www.awin1.com/cread.php?s=4069149&v=45595&q=533123&r=3008603',
       sidebar300x600: '',
+    },
+    tracking: {
+      leaderboard728x90: 'https://www.awin1.com/cread.php?s=4069162&v=45595&q=533136&r=3008603',
+      mobile320x50: 'https://www.awin1.com/cread.php?s=4069162&v=45595&q=533136&r=3008603',
+      square300x250: 'https://www.awin1.com/cread.php?s=4069149&v=45595&q=533123&r=3008603',
     },
   },
   totalFishingTackle: {
     name: 'Total Fishing Tackle',
-    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=REPLACE_ME&awinaffid=REPLACE_ME&p=https://www.totalfishingtackle.com/',
+    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=45595&awinaffid=3008603',
     ctaText: 'Shop fishing tackle & waders',
     approvedSizes: ['400x400', 'text link'],
     banners: {
-      square400x400: '',
+      square400x400: 'https://www.awin1.com/cread.php?s=4069149&v=45595&q=533123&r=3008603',
+    },
+    tracking: {
+      square400x400: 'https://www.awin1.com/cread.php?s=4069149&v=45595&q=533123&r=3008603',
     },
   },
   macdonaldHotels: {
     name: 'Macdonald Hotels',
-    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=REPLACE_ME&awinaffid=REPLACE_ME&p=https://www.macdonaldhotels.co.uk/',
+    trackingUrl: 'https://www.awin1.com/cread.php?awinmid=1963&awinaffid=3008603',
     ctaText: 'Check hotel availability near this river',
     approvedSizes: ['320x100', '320x50', '160x600', '300x600', '300x250', '728x90', 'text link'],
     banners: {
       leaderboard728x90: '',
-      mobile320x50: '',
-      square300x250: '',
-      sidebar300x600: '',
+      mobile320x50: 'https://www.awin1.com/cread.php?s=4739020&v=1963&q=587472&r=3008603',
+      square300x250: 'https://www.awin1.com/cread.php?s=4739011&v=1963&q=334515&r=3008603',
+      sidebar300x600: 'https://www.awin1.com/cread.php?s=4739018&v=1963&q=459751&r=3008603',
+    },
+    tracking: {
+      square300x250: 'https://www.awin1.com/cread.php?s=4739011&v=1963&q=334515&r=3008603',
+      sidebar300x600: 'https://www.awin1.com/cread.php?s=4739018&v=1963&q=459751&r=3008603',
+      mobile320x50: 'https://www.awin1.com/cread.php?s=4739020&v=1963&q=587472&r=3008603',
     },
   },
 };
